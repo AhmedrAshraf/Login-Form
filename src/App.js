60c2052727f4068d28e29254a71c581b94a64213
AhmedrAshraf <ahmedr.0331@gmail.com>
2024-01-27 14:27:37 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const numberOfSpans = Math.floor(window.innerHeight / 1);
+
+  const spanElements = Array.from({ length: numberOfSpans }, (_, index) => <span key={index}></span>);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+        {spanElements}
+        <div className="signin-container">
+          <div className="content">
+            <h2>Sign In</h2>
+            <div className="form">
+              <div className="inputBox">
+                <input type="text"/>
+                <i>Username</i>
+              </div>
+              <div className="inputBox">
+                <input type="password" />
+                <i>Password</i>
+              </div>
+              <div className="links">
+                <a href="#">Forgot Password ?</a>
+                <a href="#">Signup</a>
+              </div>
+              <div className="inputBox">
+                <input type="submit" value='Sign In' />
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
